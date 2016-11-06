98019b91ddb6ceca2235e66fa57d94fd6c7b4695
Jesus Mager <pywirrarika@gmail.com>
2016-11-06 00:44:49 -0600

    Segmentation works now better. Can expand postfixes better. Also dictionary and steams dic extended.

--- wix/wixaffixes.py
+++ wix/wixaffixes.py
@@ -1,6 +1,6 @@
 pre = [
-        ["'a","a", "tsi", "ke", "u","'u", "e", "'e"], # pos 18
-        ["ne", "pe", "te", "xe", "me"], #pos 17
+        ["'a","a",  "ke", "u","'u", "e", "'e"], # pos 18
+        ["ne", "pe", "te", "xe", "me", "tsi",], #pos 17
         ["ka", "r+ka", "n+ka", "ke"], #pos 16
         ["p+", "m+", "p"], #pos 15
         ["ka"], #pos 14
